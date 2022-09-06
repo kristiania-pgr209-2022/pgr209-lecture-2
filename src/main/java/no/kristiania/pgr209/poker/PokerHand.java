6f7233f68245ba266132214023eeee5c1c37ddf0
Johannes Brodwall <johannes@brodwall.com>
2022-09-06 10:53:29 +0200

    🟢 Should score pair

--- src/main/java/no/kristiania/pgr209/poker/PokerHand.java
+++ src/main/java/no/kristiania/pgr209/poker/PokerHand.java
@@ -1,5 +1,10 @@
 package no.kristiania.pgr209.poker;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PokerHand {
     private final Card[] cards;
 
@@ -8,6 +13,23 @@ public class PokerHand {
     }
 
     public String getDescription() {
+        Map<Rank, List<Card>> cardsPerRank = new HashMap<>();
+
+        for (Card card : cards) {
+            if (!cardsPerRank.containsKey(card.getRank())) {
+                cardsPerRank.put(card.getRank(), new ArrayList<>());
+            }
+            cardsPerRank.get(card.getRank()).add(card);
+        }
+
+
+        for (Rank rank : cardsPerRank.keySet()) {
+            if (cardsPerRank.get(rank).size() == 2) {
+                return "pair of " + rank.getDescription();
+            }
+        }
+
+
         Rank maxRank = Rank.NUMBER_2;
         for (Card card : cards) {
             if (card.getRank().compareTo(maxRank) > 0) {
